69e10460eb9d9d15b8b049d4811d2ce0d27428fd
bhuvanakundumani <bhuvana.kundumani@gmail.com>
2021-09-24 16:55:32 -0700

    bioelectra train script

---
+++ bioelectra_train.sh
@@ -0,0 +1,4 @@
+python drugprot_pytorch.py -output model_sep14_bioelectra_3lr_3epochs -modeltype bioelectra -overwrite true -lr 3e-5 -epochs 3
+
+python drugprot_pytorch.py -output model_sep14_bioelectra_5lr_5epochs -modeltype bioelectra -overwrite true -lr 5e-5 -epochs 5
+
